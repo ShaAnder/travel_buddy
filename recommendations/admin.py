92fec83b7cabef96ca14994c068e34566acd13ae
Shaun Anderton <shaunanderton1@gmail.com>
2025-01-22 15:42:06 +0000

    migrate model to db and admin register

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recommendation, Comment, Vote
 
 # Register your models here.
+admin.site.register(Recommendation)
+admin.site.register(Comment)
+admin.site.register(Vote)
--- travel_buddy/urls.py
+++ travel_buddy/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 
 ]
