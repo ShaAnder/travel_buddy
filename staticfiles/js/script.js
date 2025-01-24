83d2630531e3f3cec0d07f2988a9a826110a7414
Shaun Anderton <shaunanderton1@gmail.com>
2025-01-24 15:32:05 +0000

    add core templates, link svg fonts

---
+++ staticfiles/js/script.js
@@ -0,0 +1,25 @@
+// INITIALIZE ALL FUNCTIONS //
+
+// --- QUERY SELECTORS / VARS ---
+const headerToggleBtn = document.querySelector(".header-toggle");
+const header = document.querySelector("#header");
+const navLinks = document.querySelectorAll("#nav a");
+
+// Toggle the header and icon states
+const handleHeaderToggle = () => {
+    header.classList.toggle("header-show");
+    headerToggleBtn.classList.toggle("bi-list");
+    headerToggleBtn.classList.toggle("bi-x");
+};
+
+// Attach click event listener to the toggle button
+headerToggleBtn.addEventListener("click", handleHeaderToggle);
+
+// Close the nav bar if a nav link is clicked (when open)
+navLinks.forEach((nav) => {
+    nav.addEventListener("click", () => {
+        if (header.classList.contains("header-show")) {
+            handleHeaderToggle();
+        }
+    });
+});
