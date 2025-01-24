0c8c1a2f1cfe41b39db0682b20d5938150dee135
Shaun Anderton <shaunanderton1@gmail.com>
2025-01-24 15:09:32 +0000

    fix nav toggle

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,25 @@
-(function init() {
-// INIT HEADER TOGGLE //
-handleHeaderToggle = () => {
-    // find our header element and adds a toggle
-    document.querySelector("#header").classList.toggle("header-show");
+// INITIALIZE ALL FUNCTIONS //
+
+// --- QUERY SELECTORS / VARS ---
+const headerToggleBtn = document.querySelector(".header-toggle");
+const header = document.querySelector("#header");
+const navLinks = document.querySelectorAll("#nav a");
+
+// Toggle the header and icon states
+const handleHeaderToggle = () => {
+    header.classList.toggle("header-show");
     headerToggleBtn.classList.toggle("bi-list");
     headerToggleBtn.classList.toggle("bi-x");
-    // Hide the nav bar
-    document.querySelectorAll("#nav a").forEach((nav) => {
-        nav.addEventListener("click", () => {
-        if (document.querySelector(".header-show")) {
-            handleHeaderToggle();
-        }
-        });
-    });
 };
 
-})
+// Attach click event listener to the toggle button
+headerToggleBtn.addEventListener("click", handleHeaderToggle);
+
+// Close the nav bar if a nav link is clicked (when open)
+navLinks.forEach((nav) => {
+    nav.addEventListener("click", () => {
+        if (header.classList.contains("header-show")) {
+            handleHeaderToggle();
+        }
+    });
+});
