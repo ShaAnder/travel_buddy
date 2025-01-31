89c6d41c7fdf865a4c180a2ca074b5bfc222bd43
shaAnder <shaunanderton1@gmail.com>
2025-01-31 01:23:41 +0000

    begin recommendation page construction

--- recommendations/utils.py
+++ recommendations/utils.py
@@ -1,7 +1,7 @@
 import requests
 from travel_buddy.settings import GEONAME_API
 
-def get_irish_cities(username):
+def get_irish_cities():
     """
     Fetch a list of major cities in Ireland using the GeoNames API.
     
@@ -17,7 +17,7 @@ def get_irish_cities(username):
         "country": "IE",  # Ireland
         "featureClass": "P",  # Populated place
         "maxRows": 50,  # Adjust this if needed
-        "username": username
+        "username": GEONAME_API
     }
     response = requests.get(url, params=params)
     data = response.json()
