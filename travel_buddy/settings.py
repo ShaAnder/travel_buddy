569e8e198d68724818c2266c83c9920bd22fe1da
shaAnder <shaunanderton1@gmail.com>
2025-02-03 03:13:10 +0000

    set debug to false

--- travel_buddy/settings.py
+++ travel_buddy/settings.py
@@ -36,7 +36,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1:8000'
